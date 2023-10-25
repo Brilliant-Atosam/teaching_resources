17718188190314a2f8aab7c6b09b120b3f200000
Brilliant-Atosam <atosam91@gmail.com>
2023-10-25 16:04:51 +0100

    admin redesign

--- admin/src/components/UploadVideoInfoDialog.jsx
+++ admin/src/components/UploadVideoInfoDialog.jsx
@@ -57,6 +57,7 @@ export default function UploadVideoInfoDialog() {
         </DialogContent>
         <DialogActions>
           {/* <CircularProgress />  */}
+          <Button>Publish</Button>
           <Button>Next</Button>
         </DialogActions>
       </Dialog>
--- admin/src/components/uploads/Preview.jsx
+++ admin/src/components/uploads/Preview.jsx
@@ -10,10 +10,48 @@ const Preview = () => {
           className="preview-video"
           controls
         ></video>
-        <label className="choose-thumbnail" htmlFor="choose-thumbnail">
-          <BiImageAdd className="choose-thumbnail-icon" /> 
+        <label className="choose-thumbnail-label" htmlFor="choose-thumbnail">
+          <BiImageAdd className="choose-thumbnail-icon" />{" "}
+          <span>choose thumbnail</span>
         </label>
         <input type="file" id="choose-thumbnail" />
+      </div>
+      <div className="video-preview-details-container">
+        <h1 className="preview-title">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nam
+          labore soluta alias corrupti quis possimus
+        </h1>
+        <div className="preview-other-info">
+          <div className="info-key-value">
+            <span className="key">Stream:</span>
+            <span className="value">J.H.S</span>
+          </div>
+          <div className="info-key-value">
+            <span className="key">Year:</span>
+            <span className="value">Form 1</span>
+          </div>
+          <div className="info-key-value">
+            <span className="key">Price:</span>
+            <span className="value">20</span>
+          </div>
+          <div className="info-key-value">
+            <span className="key">Playlist:</span>
+            <span className="value">Lorem ipsum dolor sit.</span>
+          </div>
+        </div>
+        <div className="preview-description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          veniam nobis, ipsa excepturi quia quibusdam vitae laboriosam numquam
+          aut dolor earum, saepe fugiat dignissimos nesciunt.
+        </div>
+        <div className="preview-tags-container">
+          <span className="tag">Lorem, ipsum.</span>
+          <span className="tag">Lorem, ipsum.</span>
+          <span className="tag">Lorem, ipsum.</span>
+          <span className="tag">Lorem, ipsum.</span>
+          <span className="tag">Lorem, ipsum.</span>
+          <span className="tag">Lorem, ipsum.</span>
+        </div>
       </div>
     </div>
   );
